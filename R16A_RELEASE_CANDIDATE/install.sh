ba5fb873b078995bb2449b6b89a87f655b61229f
root <edward>
2015-04-07 13:10:17 -0700

    add 17.5 and the newy generated dockerfiles

--- R16A_RELEASE_CANDIDATE/install.sh
+++ R16A_RELEASE_CANDIDATE/install.sh
@@ -18,25 +18,8 @@ wget http://erlang.org/download/otp_src_${ERLANG_VERSION}.tar.gz -P /usr/src/erl
 cd /usr/src/erlang \
     && tar zxf otp_src_${ERLANG_VERSION}.tar.gz \
     && cd otp_src_${ERLANG_VERSION} \
-    && ./configure \
+    && ./configure --enable-threads--enable-smp-support --enable-kernel-poll --enable-fp-exceptions --enable-m64-build --with-termcap --without-javac --with-ssh --with-libatomic_ops --enable-dirty-schedulers \
     && make \
-    && make install
-
-mkdir -p /usr/src/rebar
-wget https://github.com/rebar/rebar/archive/${REBAR_VERSION}.tar.gz -P /usr/src/rebar
-cd /usr/src/rebar \
-    && tar zxf ${REBAR_VERSION}.tar.gz \
-    && cd rebar-${REBAR_VERSION} \
-    && make \
-    && cp rebar /usr/bin/rebar
-
-mkdir -p /usr/src/relx
-wget https://github.com/erlware/relx/archive/v${RELX_VERSION}.tar.gz -P /usr/src/relx
-cd /usr/src/relx \
-    && tar zxf v${RELX_VERSION}.tar.gz \
-    && cd relx-${RELX_VERSION} \
-    && make \
-    && cp relx /usr/bin/relx
-
-# Cleanup
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/src/*
+    && make release_tests \
+    && make install \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/src/*
