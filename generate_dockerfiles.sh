eab439ddfdec8f3ae408560455611abbe21b6ebc
root <edward>
2015-04-07 12:39:51 -0700

    fix some shell info

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -5,12 +5,14 @@ otp_versions="$( curl -sSL --compressed 'http://www.erlang.org/download/MD5' \
         | grep -v 'otp_src_doc' \
         | sed 's/.*(otp_src_\(.*\)\.tar\.gz).*/\1/')"
 
+versions=( "${otp_versions}" )
 
-versions=( */ )
-versions=( "${versions[@]%/}" )
+url='git://github.com/edwardt/docker-erlang'
+
+echo '# maintainer: Federico Carrone <federico.carrone@gmail.com> @(fedcarrone)'
 
 for version in ${versions}; do
         mkdir -p $version
-        sed "s/^ENV ERLANG_VERSION \(.*\)/ENV ERLANG_VERSION $version/g" base/Dockerfile > $version/Dockerfile
-        cp base/install.sh $version/install.sh
+        sed "s/^ENV ERLANG_VERSION \(.*\)/ENV ERLANG_VERSION ${version}/g" base/Dockerfile > ${version}/Dockerfile
+        cp base/install.sh ${version}/install.sh
 done
