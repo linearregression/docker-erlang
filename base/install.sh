88d43d49d00bd6b621c41aa65ea4dcbbc362f34a
root <edward>
2015-04-07 17:47:05 -0700

    enable back libatomic to test it out

--- base/install.sh
+++ base/install.sh
@@ -20,7 +20,7 @@ wget http://erlang.org/download/otp_src_${ERLANG_VERSION}.tar.gz -P /usr/src/erl
 cd /usr/src/erlang \
     && tar zxf otp_src_${ERLANG_VERSION}.tar.gz \
     && cd otp_src_${ERLANG_VERSION} \
-    && ./configure --enable-threads--enable-smp-support --enable-kernel-poll --enable-fp-exceptions --enable-m64-build --with-termcap --without-javac --with-ssh --without-libatomic_ops --enable-dirty-schedulers \
+    && ./configure --enable-threads--enable-smp-support --enable-kernel-poll --enable-fp-exceptions --enable-m64-build --with-termcap --without-javac --with-ssh --with-libatomic_ops --enable-dirty-schedulers \
     && make \
     && make release_tests \
     && make install \
